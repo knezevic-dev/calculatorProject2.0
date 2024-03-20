c251cbabd66a95af58c8f9dd6114a604f770b775
knezevic-dev <miraknezeviccc@gmail.com>
2024-03-20 14:35:47 +0100

    the change for operants that not repeat

--- calculator2.0.js
+++ calculator2.0.js
@@ -6,10 +6,23 @@ const inputField = document.getElementById('input');
 const insertNumber = number => inputField.textContent += number;
 //insert number on input field!
 
-const insertOperator = operator => inputField.textContent += operator;
-//insert operator on input field!
+const insertOperator = operator => {
+    const lastChar = inputField.textContent.slice(-1);
+    if (isOperator(lastChar)) {
+        //if last character is an operator, replace it
+        inputField.textContent = inputField.textContent.slice(0, -1) + operator;
+    } else {
+        //append the operator
+        inputField.textContent += operator;
+    }
+};
 
 const equalTo = () => (inputField.textContent) 
 ? inputField.textContent = eval(inputField.textContent) : false;
 
 const clearInput = () => inputField.textContent = '';
+
+//function that check if a character is an operator
+const isOperator = char => {
+    return ['+', '-', '*', '/'].includes(char);
+};
